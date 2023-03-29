a074f9142f6b5f1c1514ea3d2690b2c48ef3648c
Raphael Dertinatti <raphael.vedrossi@gmail.com>
2023-03-29 16:52:25 -0300

    update

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopApplication
+{
+    internal static class Program
+    {        
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            LoginScreen Login = new LoginScreen();
+            Application.Run(Login);
+
+            if (Login.LoginSucess == true)
+            {               
+                Application.Run(new Frm_Main());
+            }            
+        }
+    }
+}
