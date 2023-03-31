2bac693a03ef0046110c9b375b03f3ceacdc9cea
Raphael Dertinatti <raphael.vedrossi@gmail.com>
2023-03-31 11:38:22 -0300

    update

---
+++ Classes/cls_mysql_conn.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DesktopApplication
+{
+    public class cls_mysql_conn
+    {
+        private MySqlConnection conn;
+        private string server;        
+        private string user;
+        private string pass;
+
+        public cls_mysql_conn()
+        {
+            server = "00.0.000.000";
+            user = "system_user";
+            pass = "@aeiou0011*ABC";
+
+            string connectionString = $"datasource={server};username={user};password={pass};";
+
+            conn = new MySqlConnection(connectionString);
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                conn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public MySqlCommand CreateCommand(string query, params MySqlParameter[] parameters)
+        {
+            if (!IsConnectionOpen())
+            {
+                throw new InvalidOperationException("Não está conectado ao banco.");
+            }
+
+            try
+            {   
+                MySqlCommand cmd = new MySqlCommand(query,conn);               
+                foreach(MySqlParameter parameter in parameters)
+                {
+                    cmd.Parameters.AddWithValue(parameter.ParameterName, parameter.Value);                    
+                }
+                cmd.Prepare();
+                return cmd;
+            }
+            catch (Exception ex )
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }            
+        }
+
+        private bool IsConnectionOpen()
+        {
+            return conn != null && conn.State == System.Data.ConnectionState.Open;
+        }
+
+    }
+}
---
+++ Forms/LoginScreen.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DesktopApplication
+{
+    public partial class LoginScreen : Form
+    {
+        public bool LoginSucess = false;  
+        public static string User { get; set; }
+
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }      
+
+        private void btn_enter_Click(object sender, EventArgs e)
+        {
+            try
+            {                          
+                cls_mysql_conn connection = new cls_mysql_conn();                
+                connection.OpenConnection();
+                string sql = "SELECT * FROM db_sis.tb_users WHERE "+
+                             "USER = @user "+
+                             "AND PASS = @pass";
+
+                MySqlParameter[] parameters = new MySqlParameter[]
+                {
+                    new MySqlParameter("@user",txt_user.Text),
+                    new MySqlParameter("@pass",txt_pass.Text)
+                };              
+
+                MySqlCommand cmd = connection.CreateCommand(sql, parameters);  
+                
+                MySqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows == true)
+                {
+                    if (txt_pass.Text == "default")
+                    {
+                        Frm_NewPass newpass = new Frm_NewPass();
+                        newpass.Show();
+                    }
+                    if (txt_pass.Text != "default")
+                    {
+                        LoginSucess = true;
+                        this.Close();
+                    }
+                }
+                else
+                {                       
+                    MessageBox.Show("User/Pass incorrect, verify your credentials", "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                connection.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }            
+        }
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
